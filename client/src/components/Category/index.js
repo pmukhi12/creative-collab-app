c0ca545d222f9746bb0349e3a4b696b66ef10828
Prav Mukhi <praveshmukhi@gmail.com>
2021-09-01 16:08:08 -0400

    updated the components

--- client/src/components/Category/index.js
+++ client/src/components/Category/index.js
@@ -1,62 +1,11 @@
-import React, { useEffect } from 'react';
-import { useQuery } from '@apollo/client';
-import { useDispatch, useSelector } from 'react-redux';
-import {
-  UPDATE_CATEGORIES,
-  UPDATE_CURRENT_CATEGORY,
-} from '../../utils/actions';
-import { QUERY_CATEGORIES } from '../../utils/queries';
-import { idbPromise } from '../../utils/helpers';
+import React from 'react';
+import Auth from '../../utils/auth';
+import { Link } from 'react-router-dom';
 
-function CategoryMenu() {
-  const dispatch = useDispatch();
-  const state = useSelector((state) => state);
+function Category() {
 
-  const { categories } = state;
-
-  const { loading, data: categoryData } = useQuery(QUERY_CATEGORIES);
-
-  useEffect(() => {
-    if (categoryData) {
-      dispatch({
-        type: UPDATE_CATEGORIES,
-        categories: categoryData.categories,
-      });
-      categoryData.categories.forEach((category) => {
-        idbPromise('categories', 'put', category);
-      });
-    } else if (!loading) {
-      idbPromise('categories', 'get').then((categories) => {
-        dispatch({
-          type: UPDATE_CATEGORIES,
-          categories: categories,
-        });
-      });
-    }
-  }, [categoryData, loading, dispatch]);
-
-  const handleClick = (id) => {
-    dispatch({
-      type: UPDATE_CURRENT_CATEGORY,
-      currentCategory: id,
-    });
-  };
-
-  return (
-    <div>
-      <h2>Choose a Category:</h2>
-      {categories.map((item) => (
-        <button
-          key={item._id}
-          onClick={() => {
-            handleClick(item._id);
-          }}
-        >
-          {item.name}
-        </button>
-      ))}
-    </div>
-  );
+return <div>Category</div>
 }
 
-export default CategoryMenu;
+export default Category;
+
