b722878c62fc38b015e800e9157c6932c434d11d
Richard Mercado <RichardFM291@gmail.com>
2021-08-31 17:41:07 -0700

    modified schemas

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,10 +1,11 @@
 const { AuthenticationError } = require('apollo-server-express');
-const { User, Product, Category, Order } = require('../models');
+const { User, Event, Category, Dish} = require('../models');
 const { signToken } = require('../utils/auth');
 
 
 const resolvers = {
   Query: {
+    // pass params as a string (drop down selection for users to pick cuisines that are pulled from the data User inputs)
     categories: async () => {
       return await Category.find();
     },
@@ -21,7 +22,7 @@ const resolvers = {
         };
       }
 
-      return await EventTarget.find(params).populate('category');
+      return await Event.find(params).populate('category');
     },
     event: async (parent, { _id }) => {
       return await Event.findById(_id).populate('category');
@@ -29,30 +30,67 @@ const resolvers = {
     user: async (parent, args, context) => {
       if (context.user) {
         const user = await User.findById(context.user._id).populate({
-          path: 'myEvents.events',
-          populate: 'category'
+          // populate dishes and events
+          path: 'orders.products',
+          populate: 
         });
 
-        user.myEvents.sort((a, b) => b.eventDate - a.eventDate);
+        user.orders.sort((a, b) => b.purchaseDate - a.purchaseDate);
 
         return user;
       }
 
       throw new AuthenticationError('Not logged in');
     },
-    myEvents: async (parent, { _id }, context) => {
+    order: async (parent, { _id }, context) => {
       if (context.user) {
         const user = await User.findById(context.user._id).populate({
-          path: 'myEvents.events',
+          path: 'orders.products',
           populate: 'category'
         });
 
-        return user.myEvents.id(_id);
+        return user.orders.id(_id);
       }
 
       throw new AuthenticationError('Not logged in');
     },
+    checkout: async (parent, args, context) => {
+      const url = new URL(context.headers.referer).origin;
+      const order = new Order({ products: args.products });
+      const line_items = [];
 
+      const { products } = await order.populate('products').execPopulate();
+
+      for (let i = 0; i < products.length; i++) {
+        const product = await stripe.products.create({
+          name: products[i].name,
+          description: products[i].description,
+          images: [`${url}/images/${products[i].image}`]
+        });
+
+        const price = await stripe.prices.create({
+          product: product.id,
+          unit_amount: products[i].price * 100,
+          currency: 'usd',
+        });
+
+        line_items.push({
+          price: price.id,
+          quantity: 1
+        });
+      }
+
+      const session = await stripe.checkout.sessions.create({
+        payment_method_types: ['card'],
+        line_items,
+        mode: 'payment',
+        success_url: `${url}/success?session_id={CHECKOUT_SESSION_ID}`,
+        cancel_url: `${url}/`
+      });
+
+      return { session: session.id };
+    }
+  },
   Mutation: {
     addUser: async (parent, args) => {
       const user = await User.create(args);
