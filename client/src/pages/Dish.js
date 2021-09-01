5ffa7cf79067dba174d0faf3ca2cc7276cd2d144
Prav Mukhi <praveshmukhi@gmail.com>
2021-08-31 20:33:35 -0400

    updated the front-end pages

--- client/src/pages/Dish.js
+++ client/src/pages/Dish.js
@@ -3,7 +3,7 @@ import { Link, useParams } from 'react-router-dom';
 import { useQuery } from '@apollo/client';
 import { useDispatch, useSelector } from 'react-redux';
 
-import Cart from '../components/Cart';
+import Cart from '../components/Event';
 import {
   REMOVE_FROM_CART,
   UPDATE_CART_QUANTITY,
